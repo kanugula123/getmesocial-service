59a2f9cece7932b679c159b04ed73920972bf56c
karthikanugula <95730905+karthikanugula@users.noreply.github.com>
2022-08-28 19:39:51 -0400

    First Commit

---
+++ src/main/java/com/example/getmesocialservice/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.getmesocialservice.controller;
+
+import com.example.getmesocialservice.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/user")
+    public String getUser() {
+        return "User Information";
+    }
+}
---
+++ src/main/java/com/example/getmesocialservice/model/User.java
@@ -0,0 +1,11 @@
+package com.example.getmesocialservice.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String address;
+    private int age;
+    private String profilePicUrl;
+}
